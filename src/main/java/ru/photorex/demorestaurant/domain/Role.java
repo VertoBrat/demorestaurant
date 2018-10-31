6bd3fa2ebf348b851b1760767aafc0596d18bb32
Dima Merechko <mfocus@mail.ru>
2018-10-31 23:08:37 +0200

    init User.class , Role.class

---
+++ src/main/java/ru/photorex/demorestaurant/domain/Role.java
@@ -0,0 +1,5 @@
+package ru.photorex.demorestaurant.domain;
+
+public enum Role {
+    ROLE_USER, ROLE_ADMIN
+}
---
+++ src/main/java/ru/photorex/demorestaurant/domain/User.java
@@ -0,0 +1,38 @@
+package ru.photorex.demorestaurant.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 2, max = 100)
+    @Column(name = "user_name")
+    private String userName;
+
+    @NotBlank
+    @Size(min = 5, max = 100)
+    private String password;
+
+    @Email
+    @NotBlank
+    @Size(max = 100)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    private Set<Role> roles;
+}
